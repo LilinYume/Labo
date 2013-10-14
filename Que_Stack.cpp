c67997e09af2e0a094b0eef4489691d7ae3b5794
LilinYume <resorcin@gmail.com>
2013-10-14 16:54:11 +0900

    Update Que_Stack.cpp

--- Que_Stack.cpp
+++ Que_Stack.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 /*
-   Que char型 
-    要素数3個 固定
+   Que
+    char型 要素数3個 固定 : FIFO
     init関数によって配列の要素を全てnullで初期化しておく
     Queに3文字を超えた入力がされた場合、入力が古いものを破棄 : FIFO
      Ex : 　enq a, enq b, enq c -> |abc|
@@ -21,7 +21,7 @@ using namespace std;
    {
       char chr[SIZE];
    } Que, Stack;
-   
+
    void init();
    void enq( char dt );
    char deq();
@@ -40,30 +40,25 @@ using namespace std;
       enq( 'c' );    //  abc
       enq( 'd' );    //(a)bcd
 
-      cout << Que.chr[0] << endl;
-      cout << Que.chr[1] << endl;
-      cout << Que.chr[2] << endl;
+
+      cout << "enq(\'a\') enq(\'b\') enq(\'\\0\'); enq(\'c\'); enq(\'d\')" << endl;
+
+      cout << "Deque" << ":\t";
+      cout << deq() << ' ';
+      cout << deq() << ' ';
+      cout << deq() << endl;
 
       cout << endl;
+
+      put( 'a' );      put( 'b' );      put( 'c' );
+
+      cout << "put(\'a\') put(\'b\') put(\'c\')" << endl;
+      cout << "push from stack" << " : ";
       
-      cout << deq() << endl;
-      cout << deq() << endl;
-      cout << deq() << endl;
-      cout << deq() << endl;
+      cout << pop( ) << ' ';
+      cout << pop( ) << ' ';
+      cout << pop( ) << endl;
 
-      put( 'a' );
-      put( 'b' );
-      put( 'c' );
-
-      cout << Stack.chr[0];
-      cout << Stack.chr[1];
-      cout << Stack.chr[2];
-
-      pop( );
-      pop( );
-      pop( );
-      pop( );
-      
       return 0;
    }
    //============================================================
@@ -91,12 +86,11 @@ using namespace std;
 
       p = Que.chr + i; // 現在ポイントしている要素のアドレスを代入
 
+      // nullでなければ ポイント位置すすめ、次の要素位置のインデックスを用意
       while( *p ) 
       {
-         // nullでなければ ポイント位置すすめ、次の要素位置のインデックスを用意
          p++; 
          i++;
-
          // 配列要素がFullの場合、インデックスを先頭へ戻す
          if ( i > SIZE - 1 ) i = 0;
       }
@@ -125,16 +119,16 @@ using namespace std;
    char deq()
    {
       char value = Que.chr[ 0 ];
-      int tail = SIZE - 1;
+      int tail = SIZE - 1; 
       char *p = 0;
 
       p = Que.chr;
 
-      // Queが空っぽならnullを返して抜ける
+      // Queが空っぽなら-1を返して抜ける
       if ( !Que.chr[0] )
       {
          value = 0;
-         return value;
+         return -1;
       }
       // ポインタ位置を先頭要素にリセット
       p = Que.chr;
@@ -149,35 +143,63 @@ using namespace std;
       return value;
    }
    //============================================================
-   //                      puts()
+   //                      put()
    //============================================================
+   /* FILO */ // 末尾から追加 値は右づめで残りはnullか、値が満杯かのいずれか。
    void put( char dt )
    {
-      // インデックス先頭から値を入れる
-      // put関数を抜けてもインデックスを保持
-      static int j = 0;
 
-      // 配列要素がFullの場合、値をこれ以上入力させない。
-      if ( j >= SIZE )
-      { 
-         cout << "Over Capacity" << endl;
-         return;
+      // 先頭要素が在るとういことは既に満杯なので関数を抜ける
+      if ( Stack.chr[0] ) return;
+
+      int tail = SIZE - 1; // 配列末尾のインデックス
+      int counter = 0;     // nullの数を数える用
+
+
+      // 先頭からnullをサーチし、その数をかそえる。
+      // このループ内では、要素か空のときを想定し、末尾までnullか判定、カウントする。
+      for ( int i = 0; i < tail; i++ )
+      {
+         if ( !Stack.chr[ i ] ) counter++;
+         
       }
-      // 保持されているインデックスに値を代入
-      Stack.chr[ j ] = dt;
+      // 既に末尾に値がある場合、その分余計にカウントしたnullカウンをひとつ引く
+      if ( Stack.chr[ tail ] ) counter--;
 
-      // 次の要素がnullであれば、インデックスを一つ進める
-      if ( !Stack.chr[ j + 1 ] ) j++;
+      Stack.chr[ counter ] = dt;
+
+      //int j = 0;
+      //// 要素nullのインデックスをスキップ
+      //for ( int i = 0; Stack.chr[ i ]; i++ ) j++;
+
+      //// 配列要素がFullの場合、値をこれ以上入力させない。
+      //if ( j >= SIZE )
+      //{ 
+      //   cout << "Over Capacity" << endl;
+      //   return;
+      //}
+      //// ポイントしているインデックスに値を代入
+      //Stack.chr[ j ] = dt;
    }
    //============================================================
    //                      pop()
    //============================================================
-   /* FILO */
+   /* FILO */ // 先頭からnullではない、値を取り出す。
    char pop()
    {
-      // 配列要素を逆順に並び替える
-      // つくりかけ
-     
+      static int index = 0;
+      char fetch = 0;
 
-      return ;
+      // 値が在るインデックスをサーチ
+      if ( Stack.chr[ index ] )
+      {
+         fetch = Stack.chr[ index ];
+         index++;
+      }
+      if ( index > SIZE ) 
+      {
+         index = 0;
+         return -1;
+      }
+      return fetch;
    }
